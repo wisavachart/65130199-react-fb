3eb036cefc216ebb78a0c770753e7fca01cc9aab
Wisavachart <wisavachart.s@gmail.com>
2023-02-28 21:41:20 +0700

    Add Products

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,59 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+
+const API_FROM_RECDER = "https://six5130199-api-nodejs.onrender.com";
 export default class Product extends Component {
+  state = {
+    data: [],
+  };
+
+  getData = async () => {
+    try {
+      await axios
+        .get(`${API_FROM_RECDER}/products`)
+        .then((response) => {
+          let res = response.data;
+
+          this.setState({
+            data: res,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  componentDidMount() {
+    this.getData();
+  }
   render() {
+    const { data } = this.state;
     return (
       <div>
-        <Card>
-          <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
+        <div>
+          <h2>รายการสินค้า</h2>
+        </div>
+
+        <Row>
+          {data.map((rs, index) => (
+            <Col lg="4" md="6" sm="12" key={index}>
+              <div style={{ paddingBottom: "25px" }}>
+                <Card>
+                  <Card.Img variant="top" src={rs.cover} />
+                  <Card.Body>
+                    <Card.Title>{rs.name}</Card.Title>
+                    {rs.category}
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+          ))}
+        </Row>
       </div>
     );
   }
